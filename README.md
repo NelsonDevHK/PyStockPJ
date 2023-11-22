5ad19f1e10e8a05268d097f5c0e57712795edbb6
NelsonDevHK <songkuenhk@gmail.com>
2023-11-22 23:46:22 +0800

    [ADD]support and resistance detection file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PyStockPJ
+
+Run code with python3
+
+Don't Use yahoo API it DOESN'T WORK
---
+++ testingOnly.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('EURUSD_Candlestick_1_D_ASK_05.05.2003-19.10.2019.csv')
+
+print(df.tail())
